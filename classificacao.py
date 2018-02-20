24da71d6c7a6109721ebd9cddafed6901428b7c8
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2018-02-20 00:21:38 -0300

    Algoritmo de Classificacao Primeira Versao

---
+++ classificacao.py
@@ -0,0 +1,12 @@
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cao1 = [1, 1, 1]
+cao2 = [0, 1, 1]
+cao3 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cao1, cao2, cao3]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+misterioso = [1, 1, 1]
